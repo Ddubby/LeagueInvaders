6906d5af1c5df09de804fef184e9dca0836bcd64
Ddubby <League@AdministorsiMac.attlocal.net>
2018-03-14 18:28:36 -0700

    More Code saved

--- src/Alien.java
+++ src/Alien.java
@@ -1,4 +1,15 @@
+import java.awt.Color;
+import java.awt.Graphics;
 
-public class Alien {
+public class Alien extends GameObject {
 
+	Alien(int x, int y, int width, int height){
+	super(x, y, width, height);
+}
+void update() {
+	y=y+1;
+}
+void draw(Graphics g) {
+	g.setColor(Color.YELLOW);
+}
 }
--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,25 +1,42 @@
 import java.awt.Graphics;
 import java.util.ArrayList;
 
+import javax.swing.Timer;
+
 public class ObjectManager {
 	Rocketship rocket;
-	ArrayList <Projectile> projectiles=new ArrayList <Projectile>();
-ObjectManager(Rocketship r){
-	rocket=r;
-}
-void update(){
-	rocket.update();
-	for(Projectile p : projectiles) {
-		p.update();
+	ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
+	ArrayList<Alien> aliens = new ArrayList<Alien>();
+	ObjectManager(Rocketship r) {
+		rocket = r;
 	}
-}
-void draw(Graphics g) {
-	rocket.draw(g);
-	for(Projectile p : projectiles) {
-	p.draw(g);
-}
-}
-void addProjectile(Projectile p) {
-	projectiles.add(p);
-}
+
+	void update() {
+		rocket.update();
+		for (Projectile p : projectiles) {
+			p.update();
+		}
+		for (Alien a : aliens) {
+			a.update();
+		}
+
+	}
+
+	void draw(Graphics g) {
+		rocket.draw(g);
+		for (Projectile p : projectiles) {
+			p.draw(g);
+		}
+		for (Alien a : aliens) {
+			a.draw(g);
+		}
+	}
+
+	void addAlien(Alien a) {
+		aliens.add(a);
+	}
+
+	void addProjectile(Projectile p) {
+		projectiles.add(p);
+	}
 }
